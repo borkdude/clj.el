f31ccd7375875eec28fe7f64245950725321385d
Michiel Borkent <michielborkent@gmail.com>
2023-03-20 22:18:45 +0100

    let

--- clj.el
+++ clj.el
@@ -7,6 +7,10 @@
 (defmacro defn (name args &rest body)
   `(defun ,name ,(mapcar #'identity args) ,@body))
 
+(defmacro let (bindings &rest body)
+  `(let* ,(seq-partition (mapcar #'identity bindings) 2) ,@body))
+
+;; (let [x 1] x)
 (defn inc [x]
   (+ x 1))
 
